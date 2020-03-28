cb3b8e18b8dbc3053a2ad77c6a213f0f82a368fd
Bipeen Acharya <acharyab@seas.upenn.edu>
2020-03-28 10:56:29 -0700

    7.3: Creating first controller

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
 
@@ -12,7 +13,6 @@ var (
 	homeView    *views.View
 	contactView *views.View
 	faqView     *views.View
-	signUpView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +27,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signUpView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
@@ -49,13 +44,14 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("faq", "views/faq.gohtml")
-	signUpView = views.NewView("bootstrap", "views/signup.gohtml")
+
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 
 	var h http.Handler = http.HandlerFunc(notFound)
 	r.NotFoundHandler = h
