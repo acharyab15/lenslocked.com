21bb4a9a1313269b6af100ffc35a672bce4eea32
Bipeen Acharya <acharyab@seas.upenn.edu>
2020-04-09 22:42:33 -0700

    9.4 Creating a User

--- exp/main.go
+++ exp/main.go
@@ -1,21 +1,73 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"lenslocked.com/models"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "your-password"
+	dbname   = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name   string
+	Email  string `gorm:"not null;unique_index"`
+	Orders []Order
+}
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	data := map[string]string{
-		"Name": "John Smith",
-		"Age":  "42",
+	defer db.Close()
+	db.LogMode(true)
+
+	user := models.User{
+		Name:  "Michael Scott",
+		Email: "michael@dundermifflin.com",
 	}
-	err = t.Execute(os.Stdout, data)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
+	}
+	defer us.Close()
+	us.DestructiveReset()
+	if err := us.Create(&user); err != nil {
+		panic(err)
+	}
+
+	foundUser, err := us.ByEmail("michael@dundermifflin.com")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(foundUser)
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module lenslocked.com
 go 1.12
 
 require (
+	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.3.5 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/schema v1.1.0
+	github.com/jinzhu/gorm v1.9.12
+	github.com/jinzhu/now v1.1.1 // indirect
+	github.com/lib/pq v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/yuin/goldmark v1.1.26 // indirect
+	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	golang.org/x/sys v0.0.0-20200327173247-9dae0f8f5775 // indirect
+	golang.org/x/tools v0.0.0-20200329025819-fd4102a86c65 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
 )
