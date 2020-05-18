58c7d99d79920f4fd7fbd67031d3e1e4970eb803
Bipeen Acharya <acharyab@seas.upenn.edu>
2020-05-17 18:33:44 -0700

    14.5.4 Validating galleries

--- models/galleries.go
+++ models/galleries.go
@@ -2,9 +2,15 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+
+	ErrTitleRequired modelError = "models: title is required"
+)
+
 type Gallery struct {
 	gorm.Model
-	UserId uint   `gorm:"not_null;index"`
+	UserID uint   `gorm:"not_null;index"`
 	Title  string `gorm:"not_null"`
 }
 
@@ -42,4 +48,39 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
 }
