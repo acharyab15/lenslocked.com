2a0f3cd57ff18572fa001e2567489f800e4ba00a
Bipeen Acharya <acharyab@seas.upenn.edu>
2020-06-05 07:37:04 -0700

    15.7 Let users delete images

--- models/galleries.go
+++ models/galleries.go
@@ -10,9 +10,9 @@ const (
 
 type Gallery struct {
 	gorm.Model
-	UserID uint     `gorm:"not_null;index"`
-	Title  string   `gorm:"not_null"`
-	Images []string `gorm:"-"`
+	UserID uint    `gorm:"not_null;index"`
+	Title  string  `gorm:"not_null"`
+	Images []Image `gorm:"-"`
 }
 
 type GalleryService interface {
@@ -151,13 +151,13 @@ func (gv *galleryValidator) Delete(id uint) error {
 	return gv.GalleryDB.Delete(gallery.ID)
 }
 
-func (g *Gallery) ImagesSplitN(n int) [][]string {
+func (g *Gallery) ImagesSplitN(n int) [][]Image {
 	// Create 2D slice
-	ret := make([][]string, n)
+	ret := make([][]Image, n)
 	// Create the inner slices - we need N of them, and we will
 	// start them with a size of 0.
 	for i := 0; i < n; i++ {
-		ret[i] = make([]string, 0)
+		ret[i] = make([]Image, 0)
 	}
 	// Iterate over our images, using the index % n to determine
 	// which of the slices in ret to add the image to.
